7b63551c5735d2af6786c7d219c27605043ece77
ayub <ayubyubster@gmail.com>
2023-06-17 15:12:59 +0000

    added head.js

---
+++ head.js
@@ -0,0 +1,8 @@
+const assertEqual = require('./assertEqual.js');
+
+const head = (arr) => arr[0];
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Yellow");
+
