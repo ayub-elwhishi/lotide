099fa0fc8e30177275709454795add6bb7f17736
ayub <ayubyubster@gmail.com>
2023-06-20 00:21:11 +0000

    middle.js added

---
+++ middle.js
@@ -0,0 +1,22 @@
+function middle(arr) {
+	if (arr.length < 3) return []
+
+	const half = Math.floor(arr.length / 2)
+	if (arr.length % 2) {
+		return[arr[half]]
+	}
+	else {
+		return[arr[half - 1], arr[half]]
+	}
+}
+
+/*
+console.log(
+middle([1, 2, 3]),
+middle([1, 2, 3, 4, 5]),
+middle([1, 2, 3, 4]),
+middle([1, 2, 3, 4, 5, 6]),
+middle([1]),
+middle([1, 2])
+)
+*/
