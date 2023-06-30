0b12731a0dc236a46205fbc151d80f8631e5e1f3
ayub <ayubyubster@gmail.com>
2023-06-30 15:25:04 +0000

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,48 @@
+const assertEqual = require('./assertEqual.js');
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) 
+{
+	let result = {}
+	/*
+	for (item in itemsToCount) {
+		if (itemsToCount[item]) result[item] = 0;	
+	}
+	*/
+
+	for (item of allItems) {
+		if (itemsToCount[item]) {
+			if (result[item]) 
+			{
+				result[item] += 1		
+			}
+			else {
+				result[item] = 1		
+			}
+		}
+	}
+
+	return result
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
