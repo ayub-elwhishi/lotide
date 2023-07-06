0fc1ef2db484d1936b7766ada3d6438f0f507507
ayub <ayubyubster@gmail.com>
2023-07-06 19:22:44 +0000

    map.js

---
+++ map.js
@@ -0,0 +1,13 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  let results = [];
+  for (const item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
