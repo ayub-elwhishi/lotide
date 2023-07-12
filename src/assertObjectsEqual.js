4a302408379c85acaf3e42b65c2c5119687b414b
ayub <ayubyubster@gmail.com>
2023-07-12 18:26:38 +0000

    updated README with function descriptions

--- src/assertObjectsEqual.js
+++ src/assertObjectsEqual.js
@@ -1,7 +1,7 @@
 const eqObjects = require("./eqObjects.js");
 
-const assertObjectsEqual = function(arr1, arr2) {
-  if (eqObjects(arr1, arr2)) {
+const assertObjectsEqual = function(obj1, obj2) {
+  if (eqObjects(obj1, obj2)) {
     console.log("Assertion passed, objects equal.");
   } else {
     console.log("Assertion failed, objects unequal.");
